10cbb00959cb20d926c4afa3c822f255857e1533
WIN_NT\x16j009 <x16j009@chiba-fjb.ac.jp>
2017-05-17 12:28:28 +0900

    if~else

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,31 @@ namespace Example10
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnJudge_Click(object sender, EventArgs e)
+        {
+            int age = int.Parse(tbxAge.Text);
+            if (age >= 20)
+                lblResult.Text = "成人です";
+            else
+                lblResult.Text = "未成年です";
+            if (age == 60)
+                lblResult.Text = "還暦です";
+        }
     }
 }
